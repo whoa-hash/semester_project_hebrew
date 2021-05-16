8721d4724b263e93c279f6ed3582dd8e021f270c
Shira Orlian <59946519+whoa-hash@users.noreply.github.com>
2021-05-16 14:16:46 -0400

    added the remove nikud function for trup

--- trial1.py
+++ trial1.py
@@ -63,7 +63,16 @@ print(nltk.pos_tag(text))
 text = "בְּרֵאשִׁ֖ית בָּרָ֣א אֱלֹהִ֑ים אֵ֥ת הַשָּׁמַ֖יִם וְאֵ֥ת הָאָֽרֶץ׃"
 import subprocess
 import unicodedata
-
+def remover_nkd(text):
+    nikud = text
+    normalized = unicodedata.normalize('NFKD', nikud)  # Reduce hebrew vowel ניקוד marks
+    removed_nikud = ""
+    # since hebrew reads the other way compared to english
+    for char in range(len(normalized), 0, -1):
+        character = normalized[char - 1]
+        if not unicodedata.combining(character):
+            removed_nikud = character + removed_nikud
+    return removed_nikud
 nikud = text
 normalized = unicodedata.normalize('NFKD', nikud)  # Reduce hebrew vowel ניקוד marks
 removed_nikud = ""
