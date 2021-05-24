e3c3f1feaa906d0afc90ea7b16bfdede81e0b08f
Shira Orlian <59946519+whoa-hash@users.noreply.github.com>
2021-05-24 11:24:28 -0400

    added data text fabric

---
+++ text-fabric-data/etcbc/bhsa/tf/c/otype.tf
@@ -0,0 +1,25 @@
+@node
+@author=Eep Talstra Centre for Bible and Computer
+@dataset=BHSA
+@datasetName=Biblia Hebraica Stuttgartensia Amstelodamensis
+@email=shebanq@ancient-data.org
+@encoders=Constantijn Sikkel (QDF), and Dirk Roorda (TF)
+@valueType=str
+@version=c
+@website=https://shebanq.ancient-data.org
+@writtenBy=Text-Fabric
+@dateWritten=2018-10-08T15:11:56Z
+
+1-426584	word
+426585-426623	book
+426624-427552	chapter
+427553-515673	clause
+515674-606361	clause_atom
+606362-651541	half_verse
+651542-904748	phrase
+904749-1172289	phrase_atom
+1172290-1236016	sentence
+1236017-1300541	sentence_atom
+1300542-1414353	subphrase
+1414354-1437566	verse
+1437567-1446799	lex
---
+++ text-fabric-data/etcbc/phono/tf/c/otext@phono.tf
@@ -0,0 +1,11 @@
+@config
+@about=Provides phonetic transcriptions to Hebrew Words
+@author=BHSA Data: Constantijn Sikkel; Phono Notebook: Dirk Roorda
+@coreData=BHSA
+@coreVersion=_temp
+@fmt:text-phono-full={phono}{phono_trailer}
+@see=https://github.com/ETCBC/phono
+@source=Phono Notebook applied to BHSA Data
+@writtenBy=Text-Fabric
+@dateWritten=2018-10-08T15:16:05Z
+
